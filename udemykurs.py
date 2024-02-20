f055a820290ad1e8e1d1e12dcf5c0e9b5b26e90b
mikolajaksamit <157887790+mikolajaksamit@users.noreply.github.com>
2024-02-20 21:56:36 +0100

    Stock Prediction App

---
+++ udemykurs.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+s = pd.Series(4)
+deef= pd.Series(data=[1, 2, 3, 4, 5], index=['a', 'b', 'c', 'd', 'e'])
+print(deef)
+#%%
+deef = pd.Series(data=[1, 2, 3, 4, 5], index=['a', 'b', 'c', 'd', 'e'])
+print(deef)
