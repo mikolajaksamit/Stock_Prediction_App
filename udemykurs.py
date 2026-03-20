c155f1f253b45122ca2354dffc1957e55b481630
mikolajaksamit <157887790+mikolajaksamit@users.noreply.github.com>
2026-03-20 15:21:12 +0100

    requirements.py

--- udemykurs.py
+++ udemykurs.py
@@ -1,9 +1,7 @@
-import pandas as pd
-import numpy as np
-
-s = pd.Series(4)
-deef= pd.Series(data=[1, 2, 3, 4, 5], index=['a', 'b', 'c', 'd', 'e'])
-print(deef)
-#%%
-deef = pd.Series(data=[1, 2, 3, 4, 5], index=['a', 'b', 'c', 'd', 'e'])
-print(deef)
+pandas
+plotly
+streamlit
+yfinance
+pandas-datareader
+prophet
+ta
